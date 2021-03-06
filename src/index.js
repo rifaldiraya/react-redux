44f63866c23a03c0263b7fb994b42315d3c216b6
Rifaldi Raya <rifal.tampan50@gmail.com>
2021-03-06 09:39:29 +0700

    REDUX DONE

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-// import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,7 +13,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/pages/Tes.jsx
+++ src/pages/Tes.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { ADD_AGE } from "../redux/actionType";
 
-function Tes() {
-  return (
-    <div>
-      <p>{this.props.usia}</p>;
-    </div>
-  );
+class Tes extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.usia}</p>
+        <button onClick={this.props.handlePlus}>ADD AGE</button>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
@@ -15,4 +19,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(Tes);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handlePlus: () => dispatch({ type: ADD_AGE }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tes);
